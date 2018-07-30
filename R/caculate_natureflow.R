967ab6c7aafcb07b92df8b11811c81098f01b081
ahorawzy <lzyz09193@163.com>
2018-07-30 16:09:59 +0800

    Amend a small mistake.

--- R/caculate_natureflow.R
+++ R/caculate_natureflow.R
@@ -1,6 +1,6 @@
 #' @export
 caculate_natureflow <- function(sflinkcol,objlink){
-  t <- lapply(sflinkcol,str_subset,objlink) %>% as.character()
+  t <- as.character(lapply(sflinkcol,stringr::str_subset,objlink))
   l <- length(t[t!="character(0)"])
   return(l)
 }
