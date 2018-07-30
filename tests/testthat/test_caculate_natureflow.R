40dea3ec3092f1dd9d65cc38d4bfe39db0310d36
ahorawzy <lzyz09193@163.com>
2018-07-30 22:21:52 +0800

    Amend a little.

--- tests/testthat/test_caculate_natureflow.R
+++ tests/testthat/test_caculate_natureflow.R
@@ -5,6 +5,7 @@ df <- data.frame(a=1:3,b=letters[1:3])
 df$c[[1]] <- extract_link("1,3,5,7,8")
 df$c[[2]] <- extract_link("1,4,6,8")
 df$c[[3]] <- extract_link("1,4,5,7,9")
+
 testthat::test_that("base function can work well",{
   testthat::expect_equal(caculate_natureflow(df$c,"5-7"),2)
   testthat::expect_equal(caculate_natureflow(df$c,"1-4"),2)
