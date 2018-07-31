052675324e32c5c5b8c5abe652c766e40f5a14db
ahorawzy <lzyz09193@163.com>
2018-07-31 12:52:28 +0800

    change args order

--- R/caculate_natureflow.R
+++ R/caculate_natureflow.R
@@ -1,5 +1,5 @@
 #' @export
-caculate_natureflow <- function(sflinkcol,objlink){
+caculate_natureflow <- function(objlink,sflinkcol){
   t <- as.character(lapply(sflinkcol,stringr::str_subset,objlink))
   l <- length(t[t!="character(0)"])
   return(l)
