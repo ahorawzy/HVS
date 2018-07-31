3497526214cf36b47bb1a5ec739cf1db1f9979a3
ahorawzy <lzyz09193@163.com>
2018-07-31 14:11:21 +0800

    Amend a fatal mistake.

--- R/all_link.R
+++ R/all_link.R
@@ -3,7 +3,7 @@ all_link <- function(roadnet){
   x <- roadnet
   k <- list()
   for(i in 1:nrow(x)){
-    k[[i]] <- paste(rownames(x)[i], which(x[i,]!=0),sep = "-")
+    k[[i]] <- paste(rownames(x)[i], colnames(x)[which(x[i,]!=0)],sep = "-")
   }
   k <- unlist(k)
   return(k)
