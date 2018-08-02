7f28079b150b4e490913b100e7bc36c73c56513a
ahorawzy <lzyz09193@163.com>
2018-08-02 15:47:36 +0800

    Change Filter into where.

--- R/caculate_natureflow.R
+++ R/caculate_natureflow.R
@@ -1,7 +1,9 @@
 #' @export
 caculate_natureflow <- function(objlink,sflinkcol){
-  # t <- vapply(sflinkcol, function(x,y) y %in% x, y = objlink,logical(1))
-  # return(sum(t))
-  t <- Filter(function(x) objlink %in% x, sflinkcol)
-  return(length(t))
+  t <- where(function(x) objlink %in% x, sflinkcol)
+  return(sum(t))
+}
+
+where <- function(f, x){
+  vapply(x, f, FUN.VALUE = logical(1))
 }
