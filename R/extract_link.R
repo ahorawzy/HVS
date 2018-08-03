accae1b5fd0bd89f42fb27f87289965821ba47cf
ahorawzy <lzyz09193@163.com>
2018-08-03 11:51:39 +0800

    Add document for function extract_link().

--- R/extract_link.R
+++ R/extract_link.R
@@ -1,6 +1,15 @@
+#' Extract links from a path string.
+#'
+#' \code{extract_link} can extracts links information from a path string.
+#'
+#' @param path_i A string contains path information and seperated by ","
+#' @return A character vector contains all links in this path.
+#' @examples
+#' extract_link("1,3,5,6,7,10")
+#'
 #' @export
+#'
 extract_link <- function(path_i){
-
   p <- path_i
   p <- strsplit(p,split = ",")[[1]]
   Nlink <- length(p) - 1
