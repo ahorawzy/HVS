8342d3ebc8f6084f81d5df3ded77458e1e38f04d
ahorawzy <lzyz09193@163.com>
2018-08-03 11:26:53 +0800

    Add document for function all_link().

--- R/all_link.R
+++ R/all_link.R
@@ -1,4 +1,19 @@
+#' Print all links of a given roadnet
+#'
+#' \code{all_link} receives a roadnet matrix and print all links with sep '-'.
+#'
+#' @param roadnet A weighted-adjacent-matrix of a roadnet. Every element represents the
+#'   length of link identified by 2 points. The rownames and colnames of roadnet matrix
+#'   should also be given, which is the point index/name.
+#' @return A charactor vector contains all links in the roadnet.
+#' @examples
+#' A = matrix(c(0,2,6,0,2,0,5,7,6,5,0,1,0,7,1,0),nrow=4)
+#' rownames(A) <- 11:14
+#' colnames(A) <- 11:14
+#' all_link(A)
+#'
 #' @export
+#'
 all_link <- function(roadnet){
   x <- roadnet
   k <- list()
