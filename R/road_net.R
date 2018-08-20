b7582e4cfa3c3f9eb0a76cbeefd6c09b8780f45e
ahorawzy <lzyz09193@163.com>
2018-08-20 13:43:38 +0800

    Add data road_net of 10 points for demo.

---
+++ R/road_net.R
@@ -0,0 +1,7 @@
+#' Road net matrix for demo
+#'
+#' A demo road net matrix contains 10 points
+#'
+#' @format The matrix's is 10*10. Number "0" means there's no connection or connect to itself.
+#'
+"road_net"
