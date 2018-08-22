7c534fcb399904e46b5a64c00d7ec796a742e0a7
ahorawzy <lzyz09193@163.com>
2018-08-22 17:17:42 +0800

    Add new function to caculate flow by type or class.

--- R/caculate_natureflow.R
+++ R/caculate_natureflow.R
@@ -7,3 +7,20 @@ caculate_natureflow <- function(objlink,sflinkcol){
 where <- function(f, x){
   vapply(x, f, FUN.VALUE = logical(1))
 }
+
+#' @export
+caculate_natureflow_by <- function(sfdf,bywhat){
+  fac <- as.factor(sfdf[[bywhat]])
+  splitsfdf <- split(sfdf,fac)
+  y <- lapply(splitsfdf, caculate_flow_by)
+  z <- lapply(y, matrix)
+  z <- as.data.frame(z)
+  names(z) <- names(y)
+  return(z)
+}
+
+caculate_flow_cluster <- function(df){
+  cores <- detectCores()
+  cluster <- makePSOCKcluster(cores)
+  return(parLapply(cluster,alllink,caculate_natureflow,df$link))
+}
