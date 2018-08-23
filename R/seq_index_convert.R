d9306577e5ec67a30971e9de9fe4b0a6bdea4431
ahorawzy <lzyz09193@163.com>
2018-08-23 11:46:44 +0800

    Tidy my code.

--- R/seq_index_convert.R
+++ R/seq_index_convert.R
@@ -1,29 +1,29 @@
 #' @export
-seqtoindex <- function(seqs){
-  return(indextable[seqs])
+seqtoindex <- function(seqs) {
+    return(indextable[seqs])
 }
 
 #' @export
-indextoseq <- function(indexs){
-  return(vapply(indexs, findseq, numeric(1)))
+indextoseq <- function(indexs) {
+    return(vapply(indexs, findseq, numeric(1)))
 }
 
-findseq <- function(index){
-  if(index %in% indextable){
-    return(which(indextable == index))
-  } else {
-    return(0)
-  }
+findseq <- function(index) {
+    if (index %in% indextable) {
+        return(which(indextable == index))
+    } else {
+        return(0)
+    }
 }
 
 #' @export
-seqlinktoindexlink <- function(seqlinks){
-  return(vapply(seqlinks, findindexlink, character(1)))
+seqlinktoindexlink <- function(seqlinks) {
+    return(vapply(seqlinks, findindexlink, character(1)))
 }
 
-findindexlink <- function(seqlink){
-  seqtwopoint <- as.numeric(strsplit(seqlink,split = "-")[[1]])
-  indextwopoint <- seqtoindex(seqtwopoint)
-  indexlink <- paste(indextwopoint,collapse = "-")
-  return(indexlink)
+findindexlink <- function(seqlink) {
+    seqtwopoint <- as.numeric(strsplit(seqlink, split = "-")[[1]])
+    indextwopoint <- seqtoindex(seqtwopoint)
+    indexlink <- paste(indextwopoint, collapse = "-")
+    return(indexlink)
 }
