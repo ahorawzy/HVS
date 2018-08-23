09a76828d92512a96ab45da2f8ef3e28030c4b83
ahorawzy <lzyz09193@163.com>
2018-08-23 11:19:37 +0800

    Add new function.

--- R/caculate_natureflow.R
+++ R/caculate_natureflow.R
@@ -1,4 +1,3 @@
-#' @export
 caculate_natureflow <- function(objlink,sflinkcol){
   t <- where(function(x) objlink %in% x, sflinkcol)
   return(sum(t))
@@ -16,7 +15,24 @@ caculate_natureflow_by <- function(sfdf,bywhat){
   z <- lapply(y, matrix)
   z <- as.data.frame(z)
   names(z) <- names(y)
+  d <- data.frame(seqlink=alllink)
+  d$seqlink <- as.character(d$seqlink)
+  d$indexlink <- seqlinktoindexlink(d$seqlink)
+  d <- d[,c(2,1)]
+  z <- cbind(d,z)
   return(z)
+}
+
+#' @export
+caculate_natureflow_all <- function(sfdf){
+  l <- caculate_flow_cluster(sfdf)
+  names(l) <- alllink
+  d <- data.frame(traffic_volumn = simplify2array(l))
+  d$seqlink <- rownames(d)
+  d$indexlink <- seqlinktoindexlink(rownames(d))
+  rownames(d) <- 1:nrow(d)
+  d <- d[,c(3,2,1)]
+  return(d)
 }
 
 caculate_flow_cluster <- function(df){
